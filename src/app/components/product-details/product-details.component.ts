838ae4ad59fec98218d42c0c3f9b4ea875337ca0
lajosbarnabas0 <lajosbarnabas0@gmail.com>
2025-02-19 15:20:48 +0100

    admin

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductModel } from '../../models/product.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
+  @Input() product: ProductModel = {
+    id: 0,
+    name: '',
+    price: 0,
+    description: '',
+    imageUrl: '',
+    imageBase64: undefined,
+  };
 
+  @Output() canceled = new EventEmitter<void>();
+  @Output() saved = new EventEmitter<ProductModel>();
+  errorMessage: string = '';
+
+  constructor() {}
+
+  save(){
+    if(!this.product.name || !this.product.price || this.product.description){
+      this.errorMessage = 'A név, az ár, a termék leírás és a kép megadása kötelező!';
+      return;
+    }
+    this.saved.emit(this.product);
+  }
+
+  cancel(){
+    this.canceled.emit();
+  }
+
+  imageChanged(event: Event){
+    const input = event.target as HTMLInputElement;
+    if(input.files && input.files.length > 0){
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () =>{
+        this.product.imageBase64 = reader.result?.toString().split(',')[1];
+      }
+    }
+  }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { ProductModel } from '../../models/product.model';
+import { DataService } from '../../services/data.service';
+import { ProductDetailsComponent } from "../product-details/product-details.component";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductDetailsComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  products: ProductModel[] = [];
+  productUnderEdit: ProductModel | null = null;
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(){
+    this.dataService.getProducts().subscribe({
+      next: (response) => {
+        this.products = response;
+      },
+      error: (error) => {
+        console.error(error.error?.message || error.meesage)
+      }
+    })
+  }
+
+  new(){
+    this.productUnderEdit = {
+      id: 0,
+      name: '',
+      price: 0,
+      description: '',
+      imageUrl: '',
+      imageBase64: undefined,
+    }
+  }
+
+  update(product: ProductModel){
+    this.productUnderEdit = {...product};
+  }
+
+  delete(productId: number){
+
+  }
+
+  save(product: ProductModel){
+    const index = this.products.findIndex(p => p.id == product.id);
+    if(index == -1){
+      this.products.push(product);
+    }
+    else{
+      this.products[index] = product;
+    }
+
+    this.productUnderEdit = null;
+  }
 }
